97eb1bbba18b79046a041e4d7fa2f614467bfd4a
Karl Amoguis <ktamoguis@gmail.com>
2018-05-05 08:49:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,15 @@
 class Transfer
   # your code here
-  attr_accessor :sender
+  attr_accessor :sender, :receiver, :transferamt, :status
 
-  def initialize(sender)
+  def initialize(sender,receiver, transferamt, status = "pending")
     @sender = sender
+    @receiver = receiver
+    @transferamt = transferamt
+    @status = status
+  end
 
   end
 
-  
+
 end
