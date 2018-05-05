475683b5cfcc05a117c7114105c43bcc36085a7b
Karl Amoguis <ktamoguis@gmail.com>
2018-05-05 08:15:50 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,8 +1,11 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :balance
 
-  def initialize(name)
+  def initialize(name, balance=1000)
     @name = name
+    @balance = balance
   end
+
+  
 
 end
